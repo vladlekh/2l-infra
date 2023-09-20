d9df8843c49977a9be3367b86d3b613132e9ce33
Vladislav Leh <vladislav.leh@intexsoft.by>
2023-09-21 02:12:09 +0300

    Updated shard key

--- scripts/init-sharding.js
+++ scripts/init-sharding.js
@@ -1,5 +1,5 @@
 db.getSiblingDB('admin').auth(process.env.MONGO_USER, process.env.MONGO_PASSWORD);
 
-sh.shardCollection("l2l.products", { provider: 1, providerId: "hashed" });
+sh.shardCollection("l2l.products", { providerId: "hashed" });
 
-sh.shardCollection("l2l.images", { provider: 1, providerId: "hashed", product: 1 });
+sh.shardCollection("l2l.images", { providerId: "hashed", product: 1 });
