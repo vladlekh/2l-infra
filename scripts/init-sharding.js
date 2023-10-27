aca4b7e99f1d5033a98dd11bd4097be52fc4af15
Vladislav Leh <vladislav.leh@intexsoft.by>
2023-10-28 00:05:40 +0300

    Added jobs sharding

--- scripts/init-sharding.js
+++ scripts/init-sharding.js
@@ -1,3 +1,5 @@
 db.getSiblingDB('admin').auth(process.env.MONGO_USER, process.env.MONGO_PASSWORD);
 
 sh.shardCollection("l2l.products", {providerId: "hashed"});
+sh.shardCollection("l2l.attribute_options", {providerId: "hashed"});
+sh.shardCollection("l2l.jobs", {jobId: "hashed"});
